9be01c58c77d60b8562fb251b26518d34c1339d8
NoahNemhed <89108122+NoahNemhed@users.noreply.github.com>
2022-01-07 15:29:26 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,36 +1,75 @@
 for(let i = 0; i<5; i++){
     const row = document.createElement("p");
     const node = document.createTextNode("Rad " + (i+1));
+    
     row.appendChild(node);
-    row.style.Color = "purple";
+    row.style.Color = "#9370DB";
+    
     
     switch(i){
         case 0:
             row.style.fontSize = "15px";
-            row.style.backgroundColor = "blue"
+            row.style.backgroundColor = "#90fc03"
+            row.className = "rows";
+            row.style.width = "50%"
+            row.style.margin = "auto"
+            row.style.color = "#9370DB"
+            row.style.textAlign = "center";
+            
             break
         case 1:
             row.style.fontSize = "25px";
-            row.style.backgroundColor = "green"
+            row.style.backgroundColor = "#77fc03"
+            row.className = "rows";
+            row.style.width = "50%"
+            row.style.margin = "auto"
+            row.style.marginTop = "1rem"
+            row.style.color = "#9370DB"
+            row.style.textAlign = "center";
+            
             break
         case 2:
             row.style.fontSize = "35px";
-            row.style.backgroundColor = "red"
+            row.style.backgroundColor = "#03fc88"
+            row.className = "rows";
+            row.style.width = "50%"
+            row.style.margin = "auto"
+            row.style.marginTop = "2rem"
+            row.style.color = "#9370DB"
+            row.style.textAlign = "center";
             break
         case 3:
             row.style.fontSize = "45px";   
-            row.style.backgroundColor = "orange"
+            row.style.backgroundColor = "#03fce3"
+            row.className = "rows";
+            row.style.width = "50%"
+            row.style.margin = "auto"
+            row.style.marginTop = "3rem"
+            row.style.color = "#9370DB"
+            row.style.textAlign = "center";
             break  
 
             case 4:
-                row.style.fontSize = "45px";   
-                row.style.backgroundColor = "pink"
+                row.style.fontSize = "55px";   
+                row.style.backgroundColor = "#03b5fc"
+                row.className = "rows";
+                row.style.width = "50%"
+                row.style.margin = "auto"
+                row.style.marginTop = "4rem"
+                row.style.color = "#9370DB"
+                row.style.textAlign = "center";
                 break         
                   
     }
     document.body.appendChild(row);
     
 }
+
+const masterDiv1 = document.createElement("div");
+masterDiv1.id = "masterDiv1"
+document.body.appendChild(masterDiv1)
+
+
 
 const masterDiv = document.createElement("div");
 masterDiv.id = "masterDiv"
@@ -57,12 +96,14 @@ document.getElementById("masterDiv").appendChild(div3)
 document.getElementById("masterDiv").style.display = "flex"
 
 
+
+
 for(let i = 0; i<10; i++){
     if(i == 4){
         const row = document.createElement("p");
         const node = document.createTextNode(i);
         row.appendChild(node); 
-        row.style.backgroundColor = "purple";
+        row.style.backgroundColor = "#9370DB";
         row.style.color = "white";
         document.getElementById("div1").appendChild(row);
 
@@ -93,7 +134,7 @@ for(let i = 10; i>0; i--){
         const row = document.createElement("p");
         const node = document.createTextNode(i);
         row.appendChild(node); 
-        row.style.backgroundColor = "purple";
+        row.style.backgroundColor = "#9370DB";
         row.style.color = "white";
         document.getElementById("div2").appendChild(row);
     }
@@ -181,7 +222,7 @@ for(let i = 1; i<10; i++){
     const row = document.createElement("p");
     const node = document.createTextNode(nr);
     row.appendChild(node); 
-    row.style.backgroundColor = "purple";
+    row.style.backgroundColor = "#9370DB";
     row.style.color = "white";
     document.getElementById("div3").appendChild(row);
 
@@ -189,15 +230,21 @@ for(let i = 1; i<10; i++){
 
 }
 
-document.getElementById("masterDiv").style.justifyContent = "space-between"
+document.getElementById("masterDiv").style.justifyContent = "space-around"
 document.getElementById("masterDiv").style.border = "solid 1px black";
 document.getElementById("masterDiv").style.fontSize = "20px";
-document.getElementById("div1").style.border = "solid 3px purple";
-document.getElementById("div2").style.border = "solid 3px purple";
-document.getElementById("div3").style.border = "solid 3px purple";
-document.getElementById("div1").style.width = "33%";
-document.getElementById("div2").style.width = "33%";
-document.getElementById("div3").style.width = "33%";
+document.getElementById("div1").style.border = "solid 10px #9370DB";
+document.getElementById("div2").style.border = "solid 10px #9370DB";
+document.getElementById("div3").style.border = "solid 10px #9370DB";
+document.getElementById("div1").style.width = "5%";
+document.getElementById("div2").style.width = "5%";
+document.getElementById("div3").style.width = "5%";
 
+
+
+document.getElementById("masterDiv1").appendChild(masterDiv);
+document.getElementById("masterDiv1").style.width = "50%";
+document.getElementById("masterDiv1").style.margin = "auto"
+document.getElementById("masterDiv1").style.marginTop = "5rem"
 
 
